55d1467855b9bb35d3056b57d4d259b44ceec51e
Muhammad Hussnain <muhammadhussnain1227@gmail.com>
2026-03-01 11:35:36 +0500

    Initial Commit

---
+++ app/config.py
@@ -0,0 +1,5 @@
+MODEL_NAME = "ArcFace"
+EMBEDDINGS_PATH = "models/embeddings.pkl"
+DATASET_PATH = "dataset"
+THRESHOLD = 0.5
+CAMERA_INDEX = 0
---
+++ app/utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def cosine_similarity(a, b):
+    a = np.array(a)
+    b = np.array(b)
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+def similarity_to_confidence(similarity):
+    return round(similarity * 100, 2)
